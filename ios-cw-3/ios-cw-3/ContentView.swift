0e5f3b467d445dd947457fe4faa4c534db94fb6b
yousef moien hussain <zeroyousef13@gmail.com>
2022-03-05 14:07:32 +0300

    yousef moien hussain cw1-day2

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        ZStack{
+            Image("CAT 1")
+                .resizable()
+                .frame(width: 500, height: 130)
+            VStack{
+            Text("the nice cats")
+                VStack{
+                Image("CAT 1")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    Text("CAT1")
+               Image("CAT 2")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    Text("CAT2")
+                    HStack{
+               Image("CAT3")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                        Text("CAT3")
+        
+               Image("CAT4")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                        Text("CAT4")
+              Image("CAT5")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                       Text("CAT5")
+                }
+        }
+        }
+        
 }
-
+}
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
